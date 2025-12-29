a142befca5380bd92fcea2c01a9d3a711071b30a
ARXperience <centrodigitaldediseno@gmail.com>
2025-12-29 00:37:17 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,23 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAQnrIODc_2Qv_Snow02X-Sq8_PHwMoRVk",
-  authDomain: "trello-d2532.firebaseapp.com",
-  projectId: "trello-d2532",
-  storageBucket: "trello-d2532.firebasestorage.app",
-  messagingSenderId: "630892154656",
-  appId: "1:630892154656:web:1d0dfce355216a1d879145",
-  measurementId: "G-4L4P7E6TZC"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAQnrIODc_2Qv_Snow02X-Sq8_PHwMoRVk",
+    authDomain: "trello-d2532.firebaseapp.com",
+    projectId: "trello-d2532",
+    storageBucket: "trello-d2532.firebasestorage.app",
+    messagingSenderId: "630892154656",
+    appId: "1:630892154656:web:1d0dfce355216a1d879145",
+    measurementId: "G-4L4P7E6TZC"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
